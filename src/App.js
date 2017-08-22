8923ff400098812b66ed3b7db36477d62ab1855b
John Grant <jgrant@fandango.com>
2017-08-21 18:19:55 -0700

    added a placeholder list

--- src/App.js
+++ src/App.js
@@ -2,8 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class DataCard extends Component {
+  render() {
+    return (
+      <div>
+        <div>{this.props.logo}</div>
+        <div>{this.props.title}</div>
+      </div>
+    );
+  }
+}
+
+class ListContainer extends Component {
+  render() {
+    const elements = this.props.data.map(d => 
+        <li key={d.id}>
+          <DataCard logo={d.logo} title={d.title} />
+        </li>
+      )
+    return (
+      <ul>{elements}</ul>
+    );
+  }
+}
+
+class AddItem extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <button>add element</button>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
+    const dd = [
+      {id: 1, title: 'one', logo: 'logoOne'},
+      {id: 2, title: 'two', logo: 'logoTwo'}
+    ];
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +52,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <AddItem />
+        <ListContainer data={dd}></ListContainer>
       </div>
     );
   }

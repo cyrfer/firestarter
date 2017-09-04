9b194b28d26d392ef97b82a1842627c884b9df97
John Grant <jgrant@fandango.com>
2017-09-04 09:03:58 -0700

    rendering react on server.

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const app = express();
+// TODO: specify routes for API here
 app.get('/*', require('./dist/app').default);
 admin.initializeApp(functions.config().firebase);
 exports.app = functions.https.onRequest(app);
--- functions/src/app.js
+++ functions/src/app.js
@@ -1,5 +1,62 @@
-const foo = (req, res) => {
-    res.send('hello es6');
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { renderToString } from 'react-dom/server'
+
+const initialState = {};
+
+const appReducer = (previousState = initialState, action) => {
+    return previousState;
 };
 
-export default foo;
+const App = () => {
+    return (
+    <div>
+        <div>hello react</div>
+    </div>
+    );
+};
+
+// import appReducer from './reducers'
+// import App from './containers/App'
+// import App from '../src/App'
+
+function renderFullPage(html, preloadedState) {
+    return `
+<!doctype html>
+<html>
+    <head>
+        <title>Redux Universal Example</title>
+    </head>
+    <body>
+        <div id="root">${html}</div>
+        <script>
+// WARNING: See the following for security issues around embedding JSON in HTML:
+// http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
+window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+        </script>
+        <script src="/static/bundle.js"></script>
+    </body>
+</html>
+`
+}
+
+function handleRender(req, res) {
+    // Create a new Redux store instance
+    const store = createStore(appReducer)
+
+    // Render the component to a string
+    const html = renderToString(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    )
+
+    // Grab the initial state from our Redux store
+    const preloadedState = store.getState()
+
+    // Send the rendered page back to the client
+    res.send(renderFullPage(html, preloadedState))
+}
+
+export default handleRender;
